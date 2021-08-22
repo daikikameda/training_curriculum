08c47d3376112da62a9ceff6cb358b4002dce377
daikikameda <daiki.d.0124@gmail.com>
2021-08-22 16:24:01 +0900

    issue6解決

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,10 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
+    require 'date'
+    
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    puts wdays[0]
 
     @todays_date = Date.today
 
@@ -27,20 +30,18 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    wdays[0]
-
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @todays_date
+      wday_num = Date.today.wday
       if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(wday_num + x)] }
       @week_days.push(days)
     end
   end
